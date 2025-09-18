3ae70ae5ed3d30eb707831735196a1c32c3f39e2
yourusername <videogameacces@yahoo.com>
2025-09-17 19:48:21 -0600

    Completed up through prove

---
+++ W01/my-attempt.js
@@ -0,0 +1,9 @@
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+
+document.getElementById("myAppleElement").textContent = myApples;
+
+document.getElementById("friendAppleElement").textContent = friendApples;
+
+document.getElementById("totalAppleElement").textContent = total;
