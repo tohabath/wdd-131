bd67062cd40097d479eba4f779e8806cf1f1092b
yourusername <videogameacces@yahoo.com>
2025-11-15 22:31:26 -0700

    this is for the best

--- register/register.js
+++
@@ -1,7 +0,0 @@
-let participants = 1;
-
-function participantTemplate(count) {
-    document.querySelector('#add').insertAdjacentHTML('beforbegin', `<section class="participant${count}">`);
-}
-
-document.getElementById('add').addEventListener('click', participantTemplate);
