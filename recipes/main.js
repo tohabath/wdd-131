bf27ebf7edadecfe027f04543b2afb93a9ceec27
yourusername <videogameacces@yahoo.com>
2025-11-23 00:19:52 -0700

    recipe site finished

--- recipes/main.js
+++ recipes/main.js
@@ -1,5 +1,6 @@
 import recipes from "./recipes.mjs";
 
+
 function recipeTemplate(image, name, rating, description, getEachTag, getEachRating) {
     return `<div class="recipe-image-container">
                 <img class="recipe-image" src='${image}' alt="sweet potato waffles">
@@ -24,56 +25,29 @@ function recipeTemplate(image, name, rating, description, getEachTag, getEachRat
             </div>`;
 };
 
-function randomGet(num) {
- return Math.floor(Math.random() * num);
-};
-
-function getTags(tags) {
-    let tagSet = ``;
-    for (let tag = 0; tag < tags.length; tag++) {
-        tagSet += `<a href="" class="tag-item">
-                    ${tags[tag]}
-                </a>`
-    };
-    return tagSet;
-};
-function getRating(rating) {
-    rating = Math.floor(rating);
-    if (rating === 1) {
-        return `<span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star-empty">☆</span>
-                <span aria-hidden="true" class="icon-star-empty">☆</span>
-                <span aria-hidden="true" class="icon-star-empty">☆</span>
-                <span aria-hidden="true" class="icon-star-empty">☆</span>`
-    } else if (rating === 2) {
-        return `<span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star-empty">☆</span>
-                <span aria-hidden="true" class="icon-star-empty">☆</span>
-                <span aria-hidden="true" class="icon-star-empty">☆</span>`
-    } else if (rating === 3) {
-        return `<span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star-empty">☆</span>
-                <span aria-hidden="true" class="icon-star-empty">☆</span>`
-    } else if (rating === 4) {
-        return `<span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star-empty">☆</span>`
-    } else if (rating === 5) {
-        return `<span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star">⭐</span>`
-    };
-    
-};
-
 function renderRecipe(chosenRecipe) {
+    function getTags(tags) {
+        let tagSet = ``;
+        for (let tag = 0; tag < tags.length; tag++) {
+            tagSet += `<span class="tag-item">
+                        ${tags[tag]}
+                    </span>`
+        };
+        return tagSet;
+    };
+    function getRating(rating) {
+        rating = Math.floor(rating);
+        let ratingSpans = "";
+        for (let i = 1; i < 6; i++) {
+            if (i <= rating) {
+                ratingSpans += `<span aria-hidden="true" class="icon-star">⭐</span>`;
+            } else {
+                ratingSpans += `<span aria-hidden="true" class="icon-star-empty">☆</span>`;
+            };
+        };
+        return ratingSpans; 
+    };
+
     const image = chosenRecipe.image;
     let tags;
     if (chosenRecipe.tags) {
@@ -90,29 +64,50 @@ function renderRecipe(chosenRecipe) {
     newPost.className = "recipe";
     newPost.innerHTML = recipeTemplate(image, name, rating, description, getEachTag, getEachRating);
     const recipeContainer = document.getElementById('recipes-container');
-    recipeContainer.innerHTML = "";
     recipeContainer.append(newPost);
 }
 
+function eraseRecipes() {
+    document.getElementById('recipes-container').innerHTML = "";
+}
+
+function noRecipes() {
+    const recipeContainer = document.getElementById('recipes-container');
+    if (recipeContainer.innerHTML === "") {
+        recipeContainer.innerHTML = `<h3>Uh oh!</h3>`
+    }
+}
+
 function randomRecipe() {
+    function randomGet(num) {
+        return Math.floor(Math.random() * num);
+    };
     const chosenRecipe = recipes[randomGet(recipes.length - 1)];
+    eraseRecipes();
     return renderRecipe(chosenRecipe);
 };
 
 function filter(query) {
-    const filtered = recipes.filter(filterFunction)
-    // sort by name
-    const sorted = filtered.sort(sortFunction)
-        return sorted
+    const filtered = recipes.filter(function(recipe){
+        return recipe.name.toLowerCase().includes(query) || recipe.tags.find((item) => item.toLowerCase().includes(query)) || recipe.description.toLowerCase().includes(query)
+    });
+    const sorted = filtered.sort(function(a, b){return a.name > b.name});
+    return sorted;
 };
 
-function searchHandler(e) {
-	e.preventDefault()
-	const search = document.getElementById('searchBar').value.toLowerCase();
-    filter(search);
+function searchHandler() {
+	const searchUpper = document.getElementById('searchBar').value;
+    const search = searchUpper.toLowerCase();
+    const recipeSet = filter(search);
+    eraseRecipes();
+    for (let i = 0; i < recipeSet.length; i++) {
+        renderRecipe(recipeSet[i]);
+    };
+    noRecipes();
 };
 
 function init() {
+    document.getElementById('searchButton').addEventListener('click', (event) => {event.preventDefault(); searchHandler});
     randomRecipe();
 };
 init();
